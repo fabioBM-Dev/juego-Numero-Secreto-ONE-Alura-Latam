255de5ff897af8b4ac66bd09ab080f76d9a3e43f
H. Haru <108950779+Harizumice@users.noreply.github.com>
2025-08-15 18:53:31 -0500

    Ultimo cambio de las instrucciones

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ function verificarIntento() {
 
 function condicionesIniciales() {
     asignarTextoElemnto('h1', 'El Juego del número secreto.');
-    asignarTextoElemnto('p', `Digite un número entre 1 y ${numeroMaxJuegos}, \Tienes ${nummeroMaxIntentos} intentos...`);
+    asignarTextoElemnto('p', `Digite un número entre 1 y ${numeroMaxJuegos}, \¡¡Recuerda!! Tienes ${nummeroMaxIntentos} intentos...`);
     numeroSecreto = generarNumeroSecreto();
     intentos = 1;
 }
@@ -91,6 +91,7 @@ function generarNumeroSecreto() {
 condicionesIniciales();
 //console.log(numeroSecreto);
 //console.log(intentos);  
+
 
 
 
